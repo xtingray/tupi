2af4ba7872aaf2464aa981876f7132f9624a1786
Cristian Cepeda <cristiandavid.cepeda@gmail.com>
2012-04-08 11:24:17 -0500

    Updated install_name_tool path

--- tools/update_dylib_path.rb
+++ tools/update_dylib_path.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/ruby
 
 library = ARGV[0]
-command = "otool -L #{library}|awk '{print $1}'|grep -v '/usr/lib'|grep -v '/System'|grep \"/lib\"|grep -v '@executable'"
+
+command  = "otool -L #{library}|awk '{print $1}'|"
+command += "grep -v '/usr/lib'|grep -v '/System'|"
+command += "grep \"/lib\"|grep -v '@executable'"
 
 libs = %x[#{command}]
 puts "---------------------------------------------"
 puts "Libs for: #{library}"
 libs.each do |line|
-  parsed = line.chop().gsub(/^\/opt\/local\/lib/,"@executable_path/../Frameworks").gsub(/^\/lib/,"@executable_path/../Frameworks")
+  parsed = line.chop().gsub(/^\/lib/,"/opt/local/lib")
   cmd = "install_name_tool -change #{line.chop()} #{parsed} #{library}"
   puts cmd
   system("#{cmd}")
