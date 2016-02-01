0394a766a4f00c0137ecf74e77e75df7ea8ed721
Gustav Gonzalez <xtingray@gmail.com>
2016-02-01 12:28:23 -0500

    Adding script for Windows x64

---
+++ tools/win32/qt5_x64.bat
@@ -0,0 +1,19 @@
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\icudt55.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\icuin55.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\icuuc55.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\libgcc_s_sjlj-1.dll C:\devel\sources\qt5\
+copy "C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\libstdc++-6.dll" C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\libwinpthread-1.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Core.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Gui.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Multimedia.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5MultimediaWidgets.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Network.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5OpenGL.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\libGLESv2.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5PrintSupport.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Svg.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Widgets.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\bin\Qt5Xml.dll C:\devel\sources\qt5\
+copy C:\devel\qt-5.5.0\qt-5.5.0-x64-mingw510r0-sjlj-rev0\plugins\platforms\qwindows.dll C:\devel\sources\platforms\
+
